ae54f3107ced5c953696f3f7843b5b6c9969dbbf
alvnavra <alvnavra@gmail.com>
2018-06-24 02:18:34 +0200

    Incorporacin del alta de un actor vía consola

---
+++ src/FilmApiBundle/Command/CreateConsoleActorCommand.php
@@ -0,0 +1,41 @@
+<?php
+    namespace FilmApiBundle\Command;
+    use Symfony\Component\Console\Command\Command;
+    use Symfony\Component\Console\Input\InputInterface;
+    use Symfony\Component\Console\Output\OutputInterface;
+    use FilmApiBundle\Decorators\ActorDecorator;
+    use FilmApi\Application\Command\Actor\CreateActorCommand;
+    use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+    use Symfony\Component\Console\Input\InputArgument;
+
+    
+
+    class CreateConsoleActorCommand extends ContainerAwareCommand
+    {
+
+        protected function configure()
+        {
+            $this -> setName('app:create-actor')
+                  -> setDescription('Creates a new Actor')
+                  -> setHelp('This command allows you to create an actor')
+                  ->addArgument('actorname', InputArgument::REQUIRED, 'The name of the actor');
+
+        }
+
+        protected function execute(InputInterface $input, OutputInterface $output)
+        {
+            $output -> writeln([
+                'Actor Creator',
+                '============='
+            ]);
+
+            $name = $input->getArgument('actorname');
+            $output->writeln('Actor Name: '.$name);
+
+            $command = new CreateActorCommand($name);
+            $handler = $this->getContainer()->get('filmapi.command_handler.createActor');
+            $actor = $handler -> handle($command);
+            $this->getContainer()->get('doctrine.orm.default_entity_manager')->flush();
+
+        }
+    }
